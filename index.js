81b56355d521f5e25988dbad8022dc4a6dfbfb41
abigail <abigail.wamai@student.moringaschool.com>
2023-06-10 10:39:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const cards=(["Guadalupe", "Ollie", "Aki"],"surprise")
+
+function writeCards(cards){
+    for(let i=0; i<cards.length; i++){
+        console.log("Thank you $(cards) for the surprise gift!");
+        
+}
+return cards;
+}
+function counting(){
+    let countDown= 4;
+while (countDown<0){
+    console.log (countDown--);
+}
+}
